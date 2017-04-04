4be900b69ffdf2aa2e3e627358c7c5510b30b7c2
GreeceArtemis <greece.rapu@gmail.com>
2017-04-04 14:02:18 -0500

    v

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//window.addEventListener('load',function(){
+  //var enviar=document.getElementById("login");
+//  enviar.addEventListener('click',function(e){
+  //  e.preventDefault();
+
+
+
+//  });
+
+
+//});
+
+function validateForm(){
+	/* Escribe tú código aquí */
+
+
+
+}
